97016d1831d562038144c52ea60b66f74cf61131
AbdulrahmanAbdulhakeem <abdulrahmanabdulhakeem952@gmail.com>
2024-12-16 23:57:56 +0100

    Implemented ClusterProvider

--- templates/react/src/contexts/Providers.tsx
+++ templates/react/src/contexts/Providers.tsx
@@ -1,3 +1,4 @@
+import { ClusterProvider } from './ClusterProvider'
 import ReactQueryProvider from './ReactQueryProvider'
 import { WalletAdapterProvider } from './WalletProvider'
 
@@ -5,7 +6,9 @@ import { WalletAdapterProvider } from './WalletProvider'
 export const Providers = ({ children }: { children: React.ReactNode }) => {
   return (
     <ReactQueryProvider>
+      <ClusterProvider>
       <WalletAdapterProvider>{children}</WalletAdapterProvider>
+      </ClusterProvider>
     </ReactQueryProvider>
   )
 }
